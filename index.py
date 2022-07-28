3709361c9ccec89c9ac108fc99c22e0405fbc033
David Lopez <sebastian.830@outlook.com>
2022-07-28 08:00:40 -0500

    Comentarios

--- index.py
+++ index.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for                    #LIBRERIAS
 import os                                           #importacón de librería os
-import pymongo
+import pymongo          #libreria para la conexion con mongo
 import json
 
-MONGO_HOST="localhost"
-MONGO_PUERTO="27017"
+MONGO_HOST="localhost"          #servidor local para la conexion
+MONGO_PUERTO="27017"            #puerto para la conexion
 MONGO_TIEMPO_FUERA=1000
 
-MONGO_URI="mongodb://"+MONGO_HOST+":"+MONGO_PUERTO+"/"
+MONGO_URI="mongodb://"+MONGO_HOST+":"+MONGO_PUERTO+"/"          #concatenacion para la conexcion del mongo
 
-MONGO_BASEDATOS="Escuela"
-MONGO_COLLECTION="Profesores"
+MONGO_BASEDATOS="Escuela"           #nombre de la base de datos
+MONGO_COLLECTION="Profesores"           #nombre de la coleccion para la validacion
 cliente=pymongo.MongoClient(MONGO_URI,serverSelectionTimeoutMS=MONGO_TIEMPO_FUERA)
-baseDatos=cliente[MONGO_BASEDATOS]
-coleccionProfesor=baseDatos[MONGO_COLLECTION]
+baseDatos=cliente[MONGO_BASEDATOS]          #asigna el nombre de la bdd a una variable
+coleccionProfesor=baseDatos[MONGO_COLLECTION]      #asigna el nombre de la coleccion a una variable    
 #return a list of all collections in your database
 #print(baseDatos.list_collection_names())
 
 # Inicializar la aplicacion
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates')              #indica la carpte de los templates
 app._static_folder = os.path.abspath("templates/static/")                       #Carpeta static de los templates
 
 
@@ -28,25 +28,25 @@ def home():
     if(request.method == "POST"):
         correo = request.form['email']          #obtencion de correo
         contrase = request.form['password']         #obtencion de contraseña
-        preesccolar =request.form['preescolar'] 
+        preesccolar =request.form['preescolar']             #obtencion del preescolar
         try:
             if(coleccionProfesor.find_one({'Correo':correo, 'Contraseña': contrase, 'Preescolar': preesccolar })):        #compara los datos ingresados con el registro
-                if(preesccolar=="Preescolar 1"):
-                    return redirect(url_for('Niño')) 
+                if(preesccolar=="Preescolar 1"):        #si el preescolar es el curso 1:
+                    return redirect(url_for('Niño'))        #ingresa a la pagina niños
                 else:
-                    return redirect(url_for('NiñosB')) 
+                    return redirect(url_for('NiñosB'))  #si no al otro curso
             else:
-                return redirect(url_for('home'))   
+                return redirect(url_for('home'))            #si no esta registrado el profesor redirecciona a la pagina principal
         except:
-            return redirect(url_for('home'))
+            return redirect(url_for('home'))             #en caso de error redirecciona a la pagina principal
 
     return render_template("layouts/loginDocente.html")
 
 
-@app.route('/Niño' , methods=['GET','POST'])        
-def Niño(): 
-    if(request.method == "POST"):
-        juan = request.form['juan1']     #obtencion de correo
+@app.route('/Niño' , methods=['GET','POST'])        #ruta de la pagina niño, tiene entrada de datos por medio del metodo post
+def Niño():     #funcion que renderiza el archivo niño.html
+    if(request.method == "POST"):        #si detecta ingreso de datos por el metodo post   
+        juan = request.form['juan1']     
         sofia = request.form['sofia1']
         try:   
             print(juan)
@@ -54,31 +54,31 @@ def Niño():
             return redirect(url_for('Juego'))
         except:
             return redirect(url_for('Niño'))
-    return render_template("layouts/Niño.html")
+    return render_template("layouts/Niño.html")   #renderiza el template
 
 
-@app.route('/NiñosB' )        
-def NiñosB(): 
-    return render_template("layouts/NiñosB.html")
-
-
-
-@app.route('/Juego')        
-def Juego(): 
-    return render_template("layouts/Juego.html")
+@app.route('/NiñosB' )        #ruta de la pagina niño de otro curso,
+def NiñosB():  #funcion que renderiza el archivo niñosb.html
+    return render_template("layouts/NiñosB.html")       #renderiza el template
 
 
 
-@app.route('/Resultado', methods=['POST'])
+@app.route('/Juego')        #ruta de la pagina para el juego, 
+def Juego():    #funcion que renderiza el archivo juegos.html
+    return render_template("layouts/Juego.html")     #renderiza el template
+
+
+#funcion que recive el puntaje del archivo javascript
+@app.route('/Resultado', methods=['POST'])      #tiene entrada de datos por medio del metodo post
 def test():
     output = request.get_json()
-    result = json.loads(output) #this converts the json output to a python dictionary
-    print(result) # Printing the new dictionary
+    result = json.loads(output)     #convierte de json a diccionario de python
+    print(result) # imprime el puntake
     return result
 
 
 
-
+#main del programa
 if __name__ == '__main__':
     app.run(debug=True) # Ejecuta la aplicacion
 
--- templates/static/js/JuegoScript.js
+++ templates/static/js/JuegoScript.js
@@ -1,79 +1,87 @@
-const cuadro = document.querySelectorAll(".cuadro");
-const tiempoFaltante = document.querySelector("#tiempo");
-let puntaje = document.getElementById("puntaje");
+const cuadro = document.querySelectorAll(".cuadro"); //Constante que seleciona los cuadros creados en el HTML
+const tiempoFaltante = document.querySelector("#tiempo"); //Constante que toma tiempo ingresado en el HTML
+let puntaje = document.getElementById("puntaje"); //Variable que toma el tiempo ingresado en el HTML
 
-let resultado = 0;
-let tiempoActual = tiempoFaltante.textContent;
+let resultado = 0; //el resultado final empieza en 0, para ir incrementando
+let tiempoActual = tiempoFaltante.textContent; //variable que actualiza el tiempo 
 
+
+/*Función que mueve el topo por los diferemtes cuadros de manera que
+aleratoria, con la funicion Math.random se calcula un numero aleratorio */
 function cuadroAzar() {
     cuadro.forEach((nombreDeClase) => {
         nombreDeClase.classList.remove("topo");
     });
-    let posicionAlAzar = cuadro[Math.floor(Math.random() * 16)];
+    let posicionAlAzar = cuadro[Math.floor(Math.random() * 16)]; //calcula un numero aleratorio
     posicionAlAzar.classList.add("topo");
 
-    posicionTopo = posicionAlAzar.id;
+    posicionTopo = posicionAlAzar.id; //actualiza la posicion segun el numero al azar
 }
 
+/*Aumeta el punataje del juego segun encuentre el topo en los cuadros 
+del HTML*/
 cuadro.forEach((identificador) => {
-    identificador.addEventListener("click", () => {
+    identificador.addEventListener("click", () => { //evento que controla los clicks sobre el topo y aumenta el puntaje
         if (identificador.id === posicionTopo) {
-            resultado = resultado + 1;
+            resultado = resultado + 1; //aumenta el puntaje de 1 en 1
             puntaje.textContent = resultado;
             posicionTopo = null;
         }
     });
 });
 
+/*Funcion que mueve el topo por los cuadros cada 2 segundos*/
 function moverTopo() {
-    tiempoTopo = setInterval(cuadroAzar, 2000);
+    tiempoTopo = setInterval(cuadroAzar, 2000); //2000 milisegundos = 2 segundos
 }
 
-moverTopo();
+moverTopo(); //llama a la funcion moverTopo
 
-function actualizar() { location.reload(true); }
+function actualizar() { location.reload(true); } //funicion para recargar la pagina de manera automatia
 
+/*FUNCION para la disminucion del tiempo segun los segundos ingresados en el HTML*/
 function cuentaRegresiva() {
-    tiempoActual--;
-    tiempoFaltante.textContent = tiempoActual;
+    tiempoActual--; //el tiempo disminuye
+    tiempoFaltante.textContent = tiempoActual; //se actualiza el tiempo
 
     if (tiempoActual === 0) {
         clearInterval(idTiempo);
         clearInterval(tiempoTopo);
         /*alert("¡El tiempo ha terminado Atrapaste " + resultado + " topos!");*/
 
-        if (resultado > 3) {
-            const music = new Audio('templates/static/audio/niños.wav');
-            music.play();
-            music.loop = true;
-            music.playbackRate = 2;
+        if (resultado > 3) { //si el resultado obtentido ejecuta la siguiente instruccion 
+            const music = new Audio('templates/static/audio/niños.wav'); //carga archivo de audio
+            music.play(); //reproduce audio
+            //ALERTA DE 6 ESTRELLAS si se completan mas de 3 puntos
             Swal.fire({
                 imageUrl: 'https://static.vecteezy.com/system/resources/previews/003/795/482/non_2x/collection-of-emoticon-icon-of-cute-star-cartoon-free-vector.jpg',
-                imageWidth: 400,
-                imageHeight: 300,
-                timer: 4000,
-                showConfirmButton: false,
+                imageWidth: 400, //ancho de imagen
+                imageHeight: 300, //alto de imagen
+                timer: 4000, //asiganacion de tiempo para que cerrar alerta
+                showConfirmButton: false, //quita la confirmacion en la alerta
             });
-            music.pause();
+            music.pause(); //detiene el audio
         } else {
+            //si se completan menos de 3 putos solo obtiene 3 estrellas
             Swal.fire({
                 imageUrl: 'https://img.freepik.com/vector-premium/estrella-dibujos-animados-color-amarillo-brillante-forma-redondeada-estilo-gris-ilustracion-botones-interfaz-usuario-coleccion-estrellas-premio_505956-270.jpg?w=2000',
-                imageWidth: 400,
-                imageHeight: 300,
-                timer: 4000,
-                showConfirmButton: false,
+                imageWidth: 400, //ancho de imagen
+                imageHeight: 300, //alto de imagen
+                timer: 4000, //asiganacion de tiempo para que cerrar alerta
+                showConfirmButton: false, //quita la confirmacion en la alerta
             });
         }
 
         const dict_values = {
-            resultado
-        }
+                resultado
+            }
+            //envia el resultado obtenido hacia python por medio de ajax
         const s = JSON.stringify(dict_values);
         $.ajax({
-            url: "/Resultado",
-            type: "POST",
+            url: "/Resultado", //envia a la funcion resultado
+            type: "POST", //envia por medio del metodo post
             contentType: "application/json",
-            data: JSON.stringify(s)
+            data: JSON.stringify(s) //envia como tipo json
         });
     }
     //Función para actualizar cada n segundos(n milisegundos)
